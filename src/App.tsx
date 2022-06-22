881dd6dcf9e18ada207a8cbab9bbe1c1ec2df036
Vladyslav Polovynka <polovynka@Vladyslavs-MacBook-Pro.local>
2022-06-22 19:57:40 +0300

    first project upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Footer } from './Footer';
+
+
+
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+    <Footer copyright='C'/>
+  </div>
+)
+
+export default App;
